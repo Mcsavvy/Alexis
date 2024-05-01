d0cdd3d56e8c72bb76d76e75728674da404a2d2e
Mcsavvy <davemcsavvii@gmail.com>
2024-05-01 15:29:53 +0100

    chore: Add component to display relative time

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -25,3 +25,49 @@ export function HoverableIcon({
     />
   );
 }
+
+
+
+export function TimeAgo({ date }: { date: string}) {
+  const [timeAgoText, setTimeAgoText] = React.useState('');
+
+  function pluralizeTimeAgo(diff: number, unit: string) {
+    return `${diff} ${unit}${diff > 1 ? 's' : ''} ago`;
+  }
+
+  const calculateTimeAgo = () => {
+    const nowTs = new Date().getTime();
+    const past = new Date(date.endsWith('Z') ? date : date + 'Z');
+    // past.setTime(past.getTime() + past.getTimezoneOffset() * 60 * 1000);
+    const pastTs = past.getTime();
+    const diffInSeconds = Math.floor((nowTs - pastTs) / 1000);
+    const diffInMinutes = Math.floor(diffInSeconds / 60);
+    const diffInHours = Math.floor(diffInMinutes / 60);
+    const diffInDays = Math.floor(diffInHours / 24);
+
+    if (diffInSeconds < 60) {
+      return 'Just now';
+    } else if (diffInMinutes < 60) {
+      return pluralizeTimeAgo(diffInMinutes, 'minute');
+    } else if (diffInHours < 24) {
+      return pluralizeTimeAgo(diffInHours, 'hour');
+    } else if (diffInDays < 7) {
+      return pluralizeTimeAgo(diffInDays, 'day');
+    } else {
+      return past.toLocaleDateString();
+    }
+  };
+
+  React.useEffect(() => {
+    setTimeAgoText(calculateTimeAgo()); // Set initial state
+
+    const interval = setInterval(() => {
+      setTimeAgoText(calculateTimeAgo()); // Update state at each interval
+    }, 60000); // Update every minute
+
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, [date]); // Only rerun effect if the date prop changes
+
+  return <>{timeAgoText}</>;
+};
+
